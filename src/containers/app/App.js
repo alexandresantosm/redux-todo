5031f9f306d65f4c89a67bb8af86e969c29bc7f3
Alexandre Menezes <amenezes031@gmail.com>
2021-08-16 14:38:42 -0300

    show todo list

--- src/containers/app/App.js
+++ src/containers/app/App.js
@@ -1,3 +1,35 @@
-export default function App() {
-  return <h1>Hey, ;)</h1>;
+import { useState } from "react";
+import { connect } from "react-redux";
+
+import { Button, Input, List } from "../../components";
+import { addTodo } from "../redux/store/actions";
+
+function App(props) {
+  const [value, setValue] = useState("");
+  const { todoList, addTodo } = props;
+
+  function handleOnClick() {
+    addTodo(value);
+  }
+
+  function handleOnChange(event) {
+    event.preventDefault();
+    const { value: inputValue } = event.target;
+
+    setValue(inputValue);
+  }
+
+  return (
+    <div>
+      <List todoList={todoList} />
+      <Input onChange={(event) => handleOnChange(event)} value={value} />
+      <Button onClick={handleOnClick}>Adicionar</Button>
+    </div>
+  );
 }
+
+const mapStateToProps = (state) => ({
+  todoList: state.todo.list,
+});
+
+export default connect(mapStateToProps, { addTodo })(App);
